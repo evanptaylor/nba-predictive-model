523e68129c54331a642df305a4f7f0f435b9c23c
Evan Taylor <36122439+evanptaylor@users.noreply.github.com>
2023-04-02 18:57:11 -0400

    Add files via upload

--- nbamodel_totals.py
+++ nbamodel_totals.py
@@ -5,12 +5,15 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
 from nba_api.stats.static import teams
-from nba_api.stats.endpoints import leaguegamelog
+from nba_api.stats.endpoints import leaguegamelog, teamestimatedmetrics
 import config
 
 API_KEY = config.api_key
@@ -63,6 +66,14 @@ def fetch_odds_data(api_key):
         odds_list.append(game_data)
     return pd.DataFrame(odds_list)
 
+def fetch_game_data(start_date, end_date):
+    nba_teams = teams.get_teams()
+
+    game_log = leaguegamelog.LeagueGameLog(date_from_nullable=start_date, date_to_nullable=end_date, league_id='00')
+    games = game_log.get_data_frames()[0]
+    nba_games = games[games['TEAM_ID'].isin([team['id'] for team in nba_teams])]
+
+    return pd.DataFrame(nba_games)
 
 def fetch_team_stats_data(start_date, end_date):
     nba_teams = teams.get_teams()
@@ -70,8 +81,11 @@ def fetch_team_stats_data(start_date, end_date):
     # Fetch NBA game logs for all time
     #game_log = leaguegamelog.LeagueGameLog(date_from_nullable=start_date, date_to_nullable=end_date, league_id='00')
     game_log = leaguegamelog.LeagueGameLog(season='2022-23', league_id='00')
+    game_log_e = teamestimatedmetrics.TeamEstimatedMetrics(season='2022-23', league_id='00')
     games = game_log.get_data_frames()[0]
+    games_e = game_log_e.get_data_frames()[0]
     nba_games = games[games['TEAM_ID'].isin([team['id'] for team in nba_teams])]
+    nba_games_e = games_e[games_e['TEAM_ID'].isin([team['id'] for team in nba_teams])]
     #games_21_22 = games[games.SEASON_ID.str[-4:] == '2021']
 
     #print(nba_games)
@@ -80,6 +94,7 @@ def fetch_team_stats_data(start_date, end_date):
     for team in nba_teams:
         team_id = team['id']
         team_games = nba_games[(nba_games['TEAM_ID'] == team_id)]
+        team_games_e = nba_games_e[(nba_games_e['TEAM_ID'] == team_id)]
         #wonlast = 1 if team_games[team_games['WL'][len(team_games[team_games['WL']])-1] == 'W'] else 0
         wins = len(team_games[team_games['WL'] == 'W'])
         losses = len(team_games[team_games['WL'] == 'L'])
@@ -94,53 +109,77 @@ def fetch_team_stats_data(start_date, end_date):
         avg_oreb = team_games['OREB'].sum() / games_played if games_played > 0 else 0
         avg_ast = team_games['AST'].sum() / games_played if games_played > 0 else 0
         avg_tov =team_games['TOV'].sum() / games_played if games_played > 0 else 0
-
+        plus_minus = team_games['PLUS_MINUS'].sum() / games_played if games_played > 0 else 0
+        fgm = team_games['FGM'].sum() / games_played if games_played > 0 else 0
+        fg3m = team_games['FG3M'].sum() / games_played if games_played > 0 else 0
+        fga = team_games['FGA'].sum() / games_played if games_played > 0 else 0
+        fta = team_games['FTA'].sum() / games_played if games_played > 0 else 0
+        pace = team_games_e["E_PACE"].sum()
+        off_rating = team_games_e["E_OFF_RATING"].sum()
+        efg_pct = (fgm + 0.5*fg3m) / fga
+        ftr = fta/fga
+        net_rating = team_games_e["E_NET_RATING"].sum()
+        ast_ratio = team_games_e["E_AST_RATIO"].sum()
+        tov_pct = team_games_e["E_TM_TOV_PCT"].sum()
+        #eFG% = (FGM + 0.5 * FG3M) / FGA
         team_stat = {
             'TEAM_ID': team_id,
             'team_name': team['full_name'],
-            'games_played': games_played,
-            'wins': wins,
-            'losses': losses,
-            'win_percentage': win_percentage,
             'average_points': average_points,
             'fg3_pct': fg3_pct,
-            'fg2_pct': fg2_pct,
-            'reb':avg_reb,
             'oreb':avg_oreb,
             'ast':avg_ast,
             'tov':avg_tov,
+            'plus_minus': plus_minus,
+            'efg_pct': efg_pct,
+            'ft_rate': ftr,
+            'pace': pace,
+            'off_rating': off_rating,
+            'net_rating': net_rating,
+            'ast_ratio': ast_ratio,
+            'tov_pct':tov_pct
         }
         team_stats.append(team_stat)
     return pd.DataFrame(team_stats)
 
-def preprocess_data(odds_data, team_stats_data):
-    merged_df = odds_data.merge(team_stats_data, left_on="team1_name", right_on="team_name", suffixes=('', '_team1'))
-    merged_df = merged_df.merge(team_stats_data, left_on="team2_name", right_on="team_name", suffixes=('_team1', '_team2'))
-    merged_df = merged_df.drop(columns=['TEAM_ID_team1', 'team_name_team1', 'TEAM_ID_team2', 'team_name_team2'])
+def preprocess_data(team_stats_data, odds_data):
+
+    for team in odds_data['team1_name']:
+        merged_df = odds_data.merge(team_stats_data, left_on="team1_name", right_on="team_name", suffixes=('', '_team1'))
+        merged_df = merged_df.merge(team_stats_data, left_on="team2_name", right_on="team_name", suffixes=('_team1', '_team2'))
+        merged_df = merged_df.drop(columns=['TEAM_ID_team1', 'team_name_team1', 'TEAM_ID_team2', 'team_name_team2', 'game_id', 'team1_name','team2_name'])
 
     return merged_df
 
+    
+def make_predictions(model, X_test):
+    predictions = model.predict(X_test)
+    return predictions
 
-# Create new features
-def create_new_features(data):
-    #data['team1_points_per_game'] = data['team1_points'] / data['team1_games_played']
-    #data['team2_points_per_game'] = data['team2_points'] / data['team2_games_played']
-    return data
+def predict_upcoming_games(model, upcoming_odds_data, team_data):
+    upcoming_data = preprocess_data(team_data, odds_data)
 
-def select_features(data):
-    # Customize this function to select the features you want to use
-    features_to_drop = ['game_id', 'team1_name', 'team2_name','over_odds','under_odds']
-    return data.drop(columns=features_to_drop)
+    # Remove the 'total_points' column from the upcoming_data
+    X_upcoming = upcoming_data.drop(columns=['total_points'])
 
-# Train multiple models and combine them using a VotingClassifier
+    predictions = make_predictions(model, X_upcoming)
+    
+    # Combine the game information with predictions
+    upcoming_game_predictions = upcoming_odds_data[['team1_name', 'team2_name', 'over_odds', 'under_odds', 'total_points']].copy()
+    upcoming_game_predictions['predicted_total_points'] = predictions
+    
+    return upcoming_game_predictions
+
+
+
 def train_stacked_model(X_train, y_train):
-    rf = RandomForestClassifier()
+    rf = RandomForestRegressor()
     lr = LogisticRegression(max_iter=3000)
-    xgb = XGBClassifier()
+    xgb = XGBRegressor()
 
-    model = VotingClassifier(
+    model = VotingRegressor(
         estimators=[('rf', rf), ('lr', lr), ('xgb', xgb)],
-        voting='soft'
+        weights=[1, 1, 1]
     )
     model.fit(X_train, y_train)
     return model
@@ -153,36 +192,11 @@ def train_model(X_train, y_train):
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4]
     }
-    model = RandomForestClassifier()
-    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+    model = RandomForestRegressor()
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
     grid_search.fit(X_train, y_train)
     return grid_search.best_estimator_
 
-def make_predictions(model, X_test):
-    predictions = model.predict(X_test)
-    probabilities = model.predict_proba(X_test)
-    return predictions, probabilities
-
-def predict_upcoming_games(model, upcoming_odds_data, team_stats_data):
-    # Preprocess data
-    upcoming_data = preprocess_data(upcoming_odds_data, team_stats_data)
-
-    # Create new features
-    upcoming_data = create_new_features(upcoming_data)
-
-    # Select features
-    X_upcoming = select_features(upcoming_data)
-
-    # Make predictions
-    predictions, probabilities = make_predictions(model, X_upcoming)
-
-    # Add predictions to the DataFrame
-    upcoming_data['predicted_outcome'] = np.where(predictions == 1, "over", "under")
-    upcoming_data['over_probability'] = probabilities[:, 1]
-    upcoming_data['under_probability'] = probabilities[:, 0]
-
-
-    return upcoming_data
 
 def format_excel(file):
     # Load the workbook and select the sheet
@@ -195,82 +209,53 @@ def format_excel(file):
 
     # Iterate through the rows and apply the fill colors
     for row in range(2, ws.max_row + 1):
-        team1_prob = ws.cell(row=row, column=6).value
-        team2_prob = ws.cell(row=row, column=7).value
+        line = ws.cell(row=row, column=5).value
+        model = ws.cell(row=row, column=6).value
         
-        if team1_prob > team2_prob:
-            ws.cell(row=row, column=1).fill = winner_fill
-            ws.cell(row=row, column=2).fill = loser_fill
+        if model > line:
             ws.cell(row=row, column=3).fill = winner_fill
             ws.cell(row=row, column=4).fill = loser_fill
-            ws.cell(row=row, column=5).fill = winner_fill
-            ws.cell(row=row, column=6).fill = winner_fill
-            ws.cell(row=row, column=7).fill = loser_fill
         else:
-            ws.cell(row=row, column=1).fill = loser_fill
-            ws.cell(row=row, column=2).fill = winner_fill
             ws.cell(row=row, column=3).fill = loser_fill
             ws.cell(row=row, column=4).fill = winner_fill
-            ws.cell(row=row, column=5).fill = winner_fill
-            ws.cell(row=row, column=6).fill = loser_fill
-            ws.cell(row=row, column=7).fill = winner_fill
-    
+        
     for col in range(1, 26):
         ws.column_dimensions[chr(col + 64)].width = 20
     # Save the formatted workbook
-    wb.save("upcoming_bets.xlsx")
+    wb.save("predictions_totals.xlsx")
 
-# Main code
 if __name__ == "__main__":
-    # Define custom date range for fetching training data
-
-    start_date = "%2022-%10-%19"
-    end_date = "%2023-%03-%30"
-
-    # Fetch data
+    start_date = "2022-10-19"
+    end_date = "2023-03-30"
     odds_data = fetch_odds_data(API_KEY)
-    #odss_data = process_over_under_odds(odds_data)
-    team_stats_data = fetch_team_stats_data(start_date, end_date)
-    team_ids = team_stats_data['TEAM_ID'].unique()
-    # Preprocess data
-    data = preprocess_data(odds_data, team_stats_data)
-    # Create new features
-    data = create_new_features(data)
-    # Select features
-    X = select_features(data)
-    print(X)
-    y = np.where(data['total_points'] == 'over', 1, 0)
-
-    # Split the data into training and testing sets
+    team_data = fetch_team_stats_data(start_date, end_date)
+    data = preprocess_data(team_data, odds_data)
+    #print(odds_data)
+    #print(team_data)
+    X = data.drop(columns=['total_points'])
+    y = data['total_points']
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train the stacked model
-    model = train_stacked_model(X_train, y_train)
+    # Train the model
+    model = train_model(X_train, y_train)
 
     # Make predictions
-    predictions, probabilities = make_predictions(model, X_test)
+    predictions = make_predictions(model, X_test)
 
-    # Calculate accuracy
-    accuracy = accuracy_score(y_test, predictions)
-    print(f"Model accuracy: {accuracy}")
-
-    # Calculate confusion matrix
-    #cm = confusion_matrix(y_test, predictions)
-    #print("Confusion matrix:\n", cm)
+    # Calculate Mean Squared Error and R-squared score
+    mse = mean_squared_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    print(f"Model Mean Squared Error: {mse}")
+    print(f"Model R-squared score: {r2}")
 
     # Fetch upcoming games odds data (change the start_date and end_date to the desired range)
     upcoming_odds_data = fetch_odds_data(API_KEY)
 
     # Predict upcoming games
-    upcoming_game_predictions = predict_upcoming_games(model, upcoming_odds_data, team_stats_data)
-    
-    keep = ['team1_name', 'team2_name', 'over', 'under', 'total_points', 'predicted_outcome', 'over_probability', 'under_probability']
-    upcoming_game_predictions = upcoming_game_predictions.filter(keep)
-    
-    #print("Upcoming game predictions:\n", upcoming_game_predictions)
-    
-    upcoming_game_predictions.to_excel("predictions.xlsx", index=False)
-    format_excel("predictions.xlsx")
-    print("Done! Open predictions.xlsx to view")
-    
+    upcoming_game_predictions = predict_upcoming_games(model, upcoming_odds_data, team_data)
+    print(upcoming_game_predictions)
 
+    upcoming_game_predictions.to_excel("predictions_totals.xlsx", index=False)
+    format_excel("predictions_totals.xlsx")
+    print("Done! Open predictions_totals.xlsx to view")
